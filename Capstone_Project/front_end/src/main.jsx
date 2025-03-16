36eb077ba1e6bec12deb5b1624719d33aaba2c04
shubhya09 <shubhamsonawale111@gmail.com>
2025-03-16 12:04:20 +0530

    new capstone attachments

--- Capstone_Project/front_end/src/main.jsx
+++ Capstone_Project/front_end/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
+// eslint-disable-next-line no-unused-vars
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter} from 'react-router-dom'
+import AppContextProvider from './context/AppContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <AppContextProvider>
+        <App />
+    </AppContextProvider> 
+  </BrowserRouter>,
 )
